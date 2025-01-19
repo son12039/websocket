946d178086ecc5c1a014e64f02c679c301e73918
son12039 <sonson12039@gmail.com>
2025-01-19 23:34:13 +0900

    1

--- demo/src/main/java/com/example/Controller.java
+++ demo/src/main/java/com/example/Controller.java
@@ -1,17 +1,23 @@
 package com.example;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.log4j.Log4j2;
 
-@org.springframework.stereotype.Controller
+@RestController
 @Log4j2
+@RequestMapping("/test")
+@CrossOrigin(origins = {"*"}, maxAge = 6000)
 public class Controller {
     
     @GetMapping("/")
-    public String chatGET(){
-        log.info("@ChatController, chat GET()");
-        return "/websocket.html";
+    public ResponseEntity chatGET(){
+        log.info("췍췍");
+        return ResponseEntity.ok(null);
     }
  
 }
